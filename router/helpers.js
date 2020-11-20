31b405f78f47d0228c126d54dbada4f9ccee559a
Phil <philip-lisak@lambdastudents.com>
2020-11-19 19:58:28 -0700

    Having issues getting recipes to come back by ID

--- router/helpers.js
+++ router/helpers.js
@@ -3,28 +3,23 @@ const db = require('../data/db-config');
 module.exports = {
 	getRecipes,
 	getShoppinglist,
+	getInstructions,
 };
 
-async function getRecipes() {
-	try {
-		return db('recipes');
-	} catch (err) {
-		throw err;
-	}
+function getRecipes() {
+	return db('recipes');
 }
 
-async function getShoppinglist() {
-	try {
-		return db('recipe_components as rc')
-			.join('recipes as r', 'rc.recipe_id', 'r.id')
-			.join('ingredients as i', 'rc.ingredient_id', 'i.id')
-			.select(
-				'r.name as recipe',
-				'i.name as ingredient',
-				'rc.ingredient_amount'
-			)
-			.where({ recipe_id: recipe_id });
-	} catch (err) {
-		throw err;
-	}
+function getShoppinglist() {
+	return db('recipe_components as rc')
+		.join('recipes as r', 'rc.recipe_id', 'r.id')
+		.join('ingredients as i', 'rc.ingredient_id', 'i.id')
+		.select('r.name as recipe', 'i.name as ingredient', 'rc.ingredient_amount')
+		.where({ recipe_id: recipe_id });
+}
+
+function getInstructions() {
+	return db('recipe_components as rc')
+		.select('rc.id', 'rc.instructions')
+		.where({ recipe_id: recipe_id });
 }
--- router/recipeRouter.js
+++ router/recipeRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbConfig = require('../data/db-config');
 const Recipes = require('./helpers');
 
 const router = express.Router();
@@ -28,5 +29,19 @@ router.get('/:id/shoppinglist', (req, res) => {
 			});
 		});
 });
+
+router.get('/:id/instructions', (req, res) => {
+	const { id } = req.params
+	Recipes.getInstructions(id)
+		.then(instructions => {
+			res.status(200).json(instructions)
+		})
+		.catch(err => {
+			res.status(500).json({
+				message: 'Unable to retrieve instructions',
+				error: err
+			})
+		})
+})
 
 module.exports = router;
